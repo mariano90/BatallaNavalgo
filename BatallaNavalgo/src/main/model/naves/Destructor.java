2436b708db78b7ae64676cd613b2ed6afe867d2b
pillade <pilla.daniel91@gmail.com>
2013-05-27 23:43:49 -0300

    Adding Packages and build.xml file

--- BatallaNavalgo/src/main/model/naves/Destructor.java
+++ BatallaNavalgo/src/main/model/naves/Destructor.java
@@ -1,4 +1,4 @@
-package model;
+package main.model.naves;
 
 public class Destructor extends Nave {
 
--- BatallaNavalgo/src/main/model/naves/Lancha.java
+++ BatallaNavalgo/src/main/model/naves/Lancha.java
@@ -1,4 +1,4 @@
-package model;
+package main.model.naves;
 
 public class Lancha extends Nave {
 	
