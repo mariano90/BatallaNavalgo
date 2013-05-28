af78e8a0f1c64fedcfd29b38ec70a8ddc68ca744
Melisa <mel.poleschi@yahoo.com.ar>
2013-05-27 22:53:55 -0300

    Implementacion de las clases: Nave Buque Destructor Lancha Portaavines RompeHielos Parte DireccionesDeMovimiento

---
+++ BatallaNavalgo/src/model/Buque.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Buque extends Nave {
+
+	@Override
+	public void recibirDisparo(Disparo disparo, Parte parte){
+		
+		for( int i = 0 ; i < partes.size() ; i++ ){
+			partes.get(i).recibirDisparo();
+			partes.get(i).destruir();
+		}
+		
+	}
+}
---
+++ BatallaNavalgo/src/model/Parte.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Parte {
+	
+	private Coordenada posicion ;
+	private boolean destruida;
+	private boolean recibioDisparo; 
+	
+	
+	public Parte(){
+		this.destruida = false;
+		this.recibioDisparo = false;
+	}
+	
+	public boolean estaDestruida(){
+		return destruida;
+	}
+	
+	public boolean recibioDisparo(){
+		return recibioDisparo;
+	}
+	
+	public void recibirDisparo(){
+		this.recibioDisparo = true;
+	}
+	
+	public void destruir(){
+		this.destruida = true;
+	}
+	
+	
+	
+	
+	
+
+}
