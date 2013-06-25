203e4e3f38c2ce6d93af5757910319c65ada2e2f
Mariano <mariano@NotebookMariano.(none)>
2013-06-25 19:56:14 -0300

    Se actualiza el test

--- BatallaNavalgo/src/test/model/disparos/PorContactoTest.java
+++ BatallaNavalgo/src/test/model/disparos/PorContactoTest.java
@@ -2,7 +2,10 @@ package test.model.disparos;
 
 import main.model.disparos.Disparo;
 import main.model.disparos.PorContacto;
+import main.model.naves.Lancha;
+import main.model.naves.Nave;
 import main.model.tablero.Coordenada;
+import main.model.tablero.Tablero;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -19,8 +22,19 @@ public class PorContactoTest {
 	@Test
 	public void accionarDisparoTest(){
 		Disparo disparo = new PorContacto(new Coordenada(1, 2));
+		Nave nave = new Lancha();
+		Tablero.getTablero().getCasilleros()[1][2].agregarNave(nave);
 		boolean debeExplotar = disparo.debeExplotar();
 		Assert.assertTrue(debeExplotar);
+	}
+	
+	@Test
+	public void noAccionarDisparoTest(){
+		Disparo disparo = new PorContacto(new Coordenada(8, 9));
+		Nave nave = new Lancha();
+		Tablero.getTablero().getCasilleros()[4][5].agregarNave(nave);
+		boolean debeExplotar = disparo.debeExplotar();
+		Assert.assertFalse(debeExplotar);
 	}
 	
 }
